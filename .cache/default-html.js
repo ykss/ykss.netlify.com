86ead9434f042b09161ca318830ff6e6e7f50cdd
unknown <A679161@Mobis.HKMG.Global>
2020-04-08 14:33:02 +0900

    Fix : Google Webmaster

--- .cache/default-html.js
+++ .cache/default-html.js
@@ -2,27 +2,46 @@ import React from "react"
 import PropTypes from "prop-types"
 
 export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
+  return ( <
+    html {
+      ...props.htmlAttributes
+    } >
+    <
+    head >
+    <
+    meta charSet = "utf-8" / >
+    <
+    meta name = "google-site-verification"
+    content = "KYB5S70Dt9iQ_IdbnQ3bj_Nt_-bKEo0tDGek34L45FQ" / >
+    <
+    meta httpEquiv = "x-ua-compatible"
+    content = "ie=edge" / >
+    <
+    meta name = "viewport"
+    content = "width=device-width, initial-scale=1, shrink-to-fit=no" /
+    > {
+      props.headComponents
+    } <
+    /head> <
+    body {
+      ...props.bodyAttributes
+    } > {
+      props.preBodyComponents
+    } <
+    div key = {
+      `body`
+    }
+    id = "___gatsby"
+    dangerouslySetInnerHTML = {
+      {
+        __html: props.body
+      }
+    }
+    /> {
+      props.postBodyComponents
+    } <
+    /body> <
+    /html>
   )
 }
 
