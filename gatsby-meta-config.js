2534423b92ac828d17150c43b0fce21e05bf85a9
unknown <A679161@Mobis.HKMG.Global>
2020-04-08 16:59:43 +0900

    Add : Utterances

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -15,7 +15,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'ykss/ykss-starter-blog', // Your repository for archive comment
+    utterances: 'ykss/ykss.netlify.com', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
