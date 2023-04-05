41297a21433de0c6c8714b5fb9a8b0603088e8fd
ykss <yukyeongsang@gmail.com>
2023-04-06 01:53:06 +0900

    chore: gatsby-node.js 파일 수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,18 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
-  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`);
 
   return graphql(
     `
       {
         allMarkdownRemark(
+          filter: {
+            frontmatter: { category: { ne: null }, draft: { eq: false } }
+          }
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
@@ -21,7 +24,22 @@ exports.createPages = ({ graphql, actions }) => {
               frontmatter {
                 title
                 category
-                draft
+              }
+            }
+            previous {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+            next {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
               }
             }
           }
@@ -30,41 +48,35 @@ exports.createPages = ({ graphql, actions }) => {
     `
   ).then(result => {
     if (result.errors) {
-      throw result.errors
+      throw result.errors;
     }
 
     // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges.filter(
-      ({ node }) => !node.frontmatter.draft && !!node.frontmatter.category
-    )
-
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
+    const posts = result.data.allMarkdownRemark.edges;
+    posts.forEach(post => {
       createPage({
         path: post.node.fields.slug,
         component: blogPostTemplate,
         context: {
           slug: post.node.fields.slug,
-          previous,
-          next,
+          previous: post.next,
+          next: post.previous,
         },
-      })
-    })
-  })
-}
+      });
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
 
     createNodeField({
       name: `slug`,
       node,
       value,
-    })
+    });
   }
-}
+};
