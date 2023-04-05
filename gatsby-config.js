dc6a339bb70e17c4bf14ec4bfd7fd71b7d8549a6
ykss <yukyeongsang@gmail.com>
2023-04-06 00:43:20 +0900

    chore: node_modules 삭제

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const metaConfig = require('./gatsby-meta-config')
+const metaConfig = require('./gatsby-meta-config');
 
 module.exports = {
   siteMetadata: metaConfig,
@@ -74,6 +74,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: ['UA-116559927-2', 'G-VZ19ZNF3YQ'],
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: metaConfig.title,
@@ -100,4 +111,4 @@ module.exports = {
     `gatsby-plugin-lodash`,
     `gatsby-plugin-sitemap`,
   ],
-}
+};
