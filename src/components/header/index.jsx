ea21d17fec16bc19ff569b661a4ba1193412cc15
MK-Mac-392 <kyeongsang.yu@kurlycorp.com>
2023-04-14 14:18:34 +0900

    feature : 타이틀 영역에 visits 표시

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
-import './index.scss'
+import './index.scss';
 
 export const Header = ({ title, location, rootPath }) => {
-  const isRoot = location.pathname === rootPath
+  const isRoot = location.pathname === rootPath;
   return (
     isRoot && (
-      <h1 className="home-header">
-        <Link to={`/`} className="link">
-          {title}
-        </Link>
-      </h1>
+      <div className='home-title'>
+        <h1 className="home-header">
+          <Link to={`/`} className="link">
+            {title}
+          </Link>
+        </h1>
+        <a href="https://hits.seeyoufarm.com">
+          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fykss.netlify.app&count_bg=%234474E7&title_bg=%23697274&icon=&icon_color=%23E7E7E7&title=visits&edge_flat=false" />
+        </a>
+      </div>
     )
-  )
-}
+  );
+};
